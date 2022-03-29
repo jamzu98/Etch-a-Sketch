ae27e1a162f3bd40e0b9e539eb847f1cc1188615
Jami Makkonen <jamzu98@gmail.com>
2022-03-29 23:20:10 +0300

    Added a fill and a 'disco' functions

--- javascript.js
+++ javascript.js
@@ -4,11 +4,26 @@ const gridSizeSpan = document.querySelector('.gridSizeSpan');
 const blackButton = document.querySelector('.blackButton');
 const randomButton = document.querySelector('.randomButton');
 const copyrightYear = document.querySelector('.copyrightYear');
+const fillButton = document.querySelector('.fillButton');
+const discoButton = document.querySelector('.discoButton');
 
 let colorMode = 0;
+let partyOn = false;
+let rMode;
 
 blackButton.addEventListener('click', () => {colorMode=0; getGridSize();});
 randomButton.addEventListener('click', () => {colorMode=1; getGridSize();});
+fillButton.addEventListener('click', () => randomFill());
+discoButton.addEventListener('click', () => {
+    let partyTime;
+    if(!partyOn) {
+        discoMode();
+        partyOn = true;
+    } else {
+        stopDisco();
+        partyOn = false;
+    }
+});
 
 function drawGrid(col,rows) {
     for(let i = 0; i< (col * rows); i++) {
@@ -25,15 +40,31 @@ function drawGrid(col,rows) {
     }
 }
 
+function randomFill() {
+    getGridSize();
+    let gridboxes = document.querySelectorAll('.square');
+    gridboxes.forEach((gridbox) => {gridbox.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);});
+}
+
+function stopDisco() {
+    clearInterval(rMode);
+    discoButton.classList.remove('discoOn');
+    discoButton.textContent = 'DISCO';
+}
+
+function discoMode() {
+    rMode = setInterval(function() {randomFill();}, 300);
+    discoButton.classList.add('discoOn');
+    discoButton.textContent = 'DISCO ON';
+}
+
 function resetFunction() {
     let gridboxes = document.querySelectorAll('.square');
     gridboxes.forEach((gridbox) => {gridbox.style.backgroundColor = 'white';});
-    // console.log(gridboxes);
 }
 
 function getGridSize () {
     let sliderValue = mySlider.value;
-    // const resizeButton = document.querySelector('.resizeButton');
     const squares = document.querySelectorAll('.square');
     resetFunction();
     squares.forEach(square => square.remove());
